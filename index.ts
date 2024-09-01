8e9301813f3f13efacdace3d0e578ac0694aaf5a
Amit shochat <amitshochat66@gmail.com>
2024-09-01 21:33:42 +0300

    Add Health check for ALB

--- index.ts
+++ index.ts
@@ -38,6 +38,11 @@ app.get('/check-file', async (req: Request, res: Response) => {
   }
 });
 
+// Health check endpoint
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).send('OK');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
